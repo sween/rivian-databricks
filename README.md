b43ffbe4fdb276faf522bccf8f1a283611d67ffe
Ron Sweeney <ron.sweeney@integrationrequired.com>
2024-01-28 07:07:48 -0500

    more stuff

--- README.md
+++ README.md
@@ -3,7 +3,27 @@ Supporting assets for Rivian Databricks Lakehouse
 
 <img src="https://github.com/sween/rivian-databricks/raw/main/assets/rivian-databricks.png" alt="Aspiring Brick Builder">
 
-Documentation and Errata over @ [Deez Watts - A Rivian Data Adventure](https://www.deezwatts.com)
+Create a namespace and add your Rivian Credentials as a secret:
+
+```
+kubectl create ns rivian
+kubectl create secret generic rivian-user-pass -n rivian \
+    --from-literal=rivian_username='ron.sweeney+api@hotmale.com' \
+    --from-literal=rivian_password='12345' # same as your luggage
+
+kubectl create secret generic aws-creds -n rivian \
+    --from-literal=aws_access_key_id='AKIA.....' \
+    --from-literal=aws_secret_access_key='12345' \ # same as your luggage
+    --from-literal=aws_default_region='us-east-2'
+```
+
+Apply the Deployment
+
+```
+kubectl apply -f deploy/deployment.yaml -n rivian
+```
+
+More Documentation and Errata over @ [Deez Watts - A Rivian Data Adventure](https://www.deezwatts.com)
 
 ## Props
 The Mace [the-mace/rivian-python-api](https://github.com/the-mace/rivian-python-api)  
--- src/rivian_deezwatts.py
+++ src/rivian_deezwatts.py
@@ -2,46 +2,64 @@ import rivian_api as rivian
 import os
 import json
 import boto3
+import time
 from datetime import datetime
 
-rivian = rivian.Rivian()
+
+class RivianDataBricks(object):
+    def __init__(self):
+        self.rivian = rivian.Rivian()
+        response = self.rivian.login(
+            os.environ['RIVIAN_USERNAME'],
+            os.environ['RIVIAN_PASSWORD']
+        )
+        # owner info, grab rivian vehicleid
+        self.owner = self.rivian.get_user_information()
+        self.rivianid = self.owner['data']['currentUser']['vehicles'][0]['id']
+        print(f'Rivian: {self.rivianid}')
+
+    def run(self):
+        # status info
+        whipstatus = self.rivian.get_vehicle_state(self.rivianid)
+        # whip info
+        whip = self.rivian.get_vehicle(self.rivianid)
+
+        # charging info
+        charge = self.rivian.get_live_session_data(self.rivianid)
+        charge_schedule = self.rivian.get_charging_schedule(self.rivianid)
+
+        charge['charge_schedule'] = charge_schedule
+
+        # ota details
+        ota = self.rivian.get_ota_details(self.rivianid)
+
+        # last connection
+        last_connection = self.rivian.get_vehicle_last_connection(self.rivianid)
 
 
-response = rivian.login(
-    dbutils.secrets.get(scope="deezwatts", key="username"),
-    dbutils.secrets.get(scope="deezwatts", key="password")
+        # status is our main dictionary, add the other two keys
+        # and make a gigantic complex json object for databricks
+        whipstatus['whip'] = whip
+        whipstatus['charge'] = charge
+        whipstatus['owner'] = self.owner
+        whipstatus['last_connection'] = last_connection
 
-)
+        deezwatts = json.dumps(whipstatus)
 
-# owner info, grab rivian vehicleid
-owner = rivian.get_user_information()
-rivianid = owner['data']['currentUser']['vehicles'][0]['id']
-print(f'Rivian: {rivianid}')
-
-# status info
-whipstatus = rivian.get_vehicle_state(rivianid)
-
-# whip info
-whip = rivian.get_vehicle(rivianid)
-
-# charging info
-charge = rivian.get_live_session_data(rivianid)
-
-# status is our main dictionary, add the other two keys
-whipstatus['whip'] = whip
-whipstatus['charge'] = charge
-
-deezwatts = json.dumps(whipstatus)
-
-timeitis = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+        timeitis = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
 
 
-# upload to a bucket
-s3 = boto3.resource('s3')
-s3object = s3.Object('deezwatts', 'in/json/deezwatts-json-' + timeitis + '.json')
+        # upload to a bucket
+        s3 = boto3.resource('s3')
+        s3object = s3.Object('deezwatts', 'in/json/deezwatts-json-' + timeitis + '.json')
 
-response = s3object.put(
-    Body=(bytes(deezwatts.encode('UTF-8')))
-)
+        response = s3object.put(
+            Body=(bytes(deezwatts.encode('UTF-8')))
+        )       
 
 
+if __name__ == '__main__':
+    while True:
+        RivianDataBricks().run()
+        # lets not piss off the Site Reliability Teams at Rivian
+        time.sleep(120)
